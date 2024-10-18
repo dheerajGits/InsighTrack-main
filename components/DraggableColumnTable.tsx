4bb82298d11a0de83f131533daf93aa9fb4a009f
Dheeraj202 <dheerajsadan2002@gmail.com>
2024-10-18 23:51:21 +0530

    change table styling and remove test data

--- components/DraggableColumnTable.tsx
+++ components/DraggableColumnTable.tsx
@@ -1,26 +1,22 @@
 "use client";
 
 import React, { useState } from "react";
+import { MdDragIndicator } from "react-icons/md";
 
 // Initial column and row data
-const initialColumns = [
+type column = {
+  id: string;
+  label: string;
+};
+const initialColumns: column[] = [
   { id: "name", label: "Name" },
-  { id: "age", label: "Age" },
+  { id: "distinct_id", label: "Distinct Id" },
   { id: "email", label: "Email" },
   { id: "phone", label: "Phone" },
+  { id: "raw_data", label: "Raw Data" },
 ];
 
-const initialRows = [
-  { name: "John Doe", age: 28, email: "john@example.com", phone: "123456789" },
-  {
-    name: "Jane Smith",
-    age: 34,
-    email: "jane@example.com",
-    phone: "987654321",
-  },
-];
-
-const DraggableTable = () => {
+const DraggableTable = (users: any) => {
   const [columns, setColumns] =
     useState<{ id: string; label: string }[]>(initialColumns);
   const [draggedColumnIndex, setDraggedColumnIndex] = useState<any>();
@@ -37,6 +33,8 @@ const DraggableTable = () => {
 
   // Function to handle drop and reorder columns
   const handleDrop = (index: number) => {
+    if (draggedColumnIndex === null) return;
+
     const draggedColumn = columns[draggedColumnIndex];
     const updatedColumns = [...columns];
 
@@ -48,30 +46,38 @@ const DraggableTable = () => {
     setColumns(updatedColumns);
     setDraggedColumnIndex(null); // Reset dragged index after dropping
   };
-
   return (
     <table className="border-1">
       <thead>
-        <tr>
+        <tr className=" bg-[#3c3c3c]">
           {columns.map((column, index) => (
             <th
               key={column.id}
-              draggable
-              onDragStart={() => handleDragStart(index)}
               onDragOver={handleDragOver}
               onDrop={() => handleDrop(index)}
-              className="cursor-move"
+              className="p-2 relative  rounded-l-xl "
             >
-              {column.label}
+              {/* Column Label */}
+              <p className="max-w-20">{column.label}</p>
+              {/* Drag Icon */}
+              <span
+                draggable
+                onDragStart={() => handleDragStart(index)}
+                className="absolute right-2 top-2 cursor-move"
+              >
+                <MdDragIndicator size={20} />
+              </span>
             </th>
           ))}
         </tr>
       </thead>
-      <tbody>
-        {initialRows.map((row: any, rowIndex) => (
-          <tr key={rowIndex}>
+      <tbody className="mt-2">
+        {users.map((row: any, rowIndex: number) => (
+          <tr key={rowIndex} className="hover:bg-gray-700 rounded-lg">
             {columns.map((column: { id: string; label: string }) => (
-              <td key={column.id}>{row[column.id]}</td>
+              <td key={column.id} className="pl-4 py-2">
+                {row[column.id]}
+              </td>
             ))}
           </tr>
         ))}
--- components/UsersTable.tsx
+++ components/UsersTable.tsx
@@ -4,7 +4,7 @@ import DraggableTable from "./DraggableColumnTable";
 
 export default function UserTable({ users }: { users: any }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mt-10 border-gray-500 border-2 rounded-lg">
       <DraggableTable />
     </div>
   );
